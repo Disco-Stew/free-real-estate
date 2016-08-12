4f3959fcfb343f86d5f67cc6db3f0f4417912645
Disco-Stew <wcampbell07@qub.ac.uk>
2016-08-12 13:58:30 -0700

    Add files via upload

---
+++ free-realestate/popup.js
@@ -0,0 +1,11 @@
+var audio = new Audio('freerealestate.mp3');
+audio.play();
+
+// this.currentTime = 12;
+// audio.addEventListener("canplaythrough", function () {
+//         setTimeout(function(){
+//             audio.pause();
+//             alert("Audio Stop Successfully");
+//         },
+//         2500);
+// }, false);
